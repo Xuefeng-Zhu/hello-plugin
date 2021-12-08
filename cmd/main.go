23b773be0b3e5e612932671f8af0dcc6ad8753cc
Xuefeng Zhu <xzhu15@illinois.edu>
2021-12-07 20:30:12 -0800

    update readme

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func main() {
+
+}
+
+func NewDocs() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "hello",
+		Short: "Say hello",
+	}
+
+	worldCmd := &cobra.Command{
+		Use:   "world",
+		Short: "Hello world",
+		RunE:  hellowWorldHandler,
+	}
+
+	c.AddCommand(worldCmd)
+	return c
+}
+
+func hellowWorldHandler(cmd *cobra.Command, args []string) error {
+	cmd.Print("Hello world")
+
+	return nil
+}
